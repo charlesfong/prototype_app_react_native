335ba3786524ea3c60efa74e1f8cdf9a793433c5
charlesfong <47769622+charlesfong@users.noreply.github.com>
2019-08-26 16:38:43 +0700

    albert

--- src/modules/AppView.js
+++ src/modules/AppView.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Navigator from './navigation/Navigator';
 
-export default function AppView(props) {
-  console.warn(props.item);
+export default function AppView() {
   return <Navigator onNavigationStateChange={() => {}} uriPrefix="/app" />;
 }
